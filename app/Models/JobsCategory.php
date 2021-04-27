90da7af43a5b665fb86a25ed7c128b5f76802d0a
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-04-27 16:45:41 +0200

    Update JobsCategory.php

--- app/Models/JobsCategory.php
+++ app/Models/JobsCategory.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Jobs;
 
 class JobsCategory extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['category'];
+
+    public function jobs(){
+        return $this->hasOne(Jobs::class);
+    }
 }
