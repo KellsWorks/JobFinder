f56394f31ad83f6fac13b707453df8060e2185e1
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-05-10 12:35:17 +0200

    init🎉🎉🎉🎉

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,10 +19,20 @@ class HomeController extends Controller
     /**
      * Show the application dashboard.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
     {
         return view('home');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function adminHome()
+    {
+        return view('adminHome');
     }
 }
