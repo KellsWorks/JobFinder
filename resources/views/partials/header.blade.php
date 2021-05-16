25fc9f55ed60f13345d50e9b9d3225711332d216
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-05-17 00:24:42 +0200

    Update header.blade.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,7 +3,7 @@
         <nav class="navbar site-navbar offcanvas-active navbar-expand-lg  px-0 py-0">
 
             <div class="brand-logo">
-                <a href="{{ url('/') }}">
+                <a href="{{ route('index') }}">
                     <h3>
                         <span class="text-green">JOB</span> FINDER
                     </h3>
@@ -16,7 +16,7 @@
                             <a class="nav-link" aria-expanded="false">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{ url('jobs') }}" class="nav-link">JOBS</a>
+                            <a href="{{ route('jobs') }}" class="nav-link">JOBS</a>
                         </li>
 
                         <li class="nav-item">
@@ -29,20 +29,30 @@
                 </button>
             </div>
             <div class="header-btns header-btn-devider ml-auto pr-2 ml-lg-6 d-none d-xs-flex">
+                @if(Route::has('login'))
+                <div class="absolute top-0 right-0 mt-4 mr-4 space-x-4 sm:mt-6 sm:mr-6 sm:space-x-6">
+                @auth
+                <a href="{{ url('/home') }}" class="no-underline hover:underline text-sm font-normal text-teal-800 uppercase">{{ __('Home') }}</a>
+                @else
                 <a class="btn btn-transparent text-uppercase font-size-3 heading-default-color focus-reset" href="javacript:" data-toggle="modal" data-target="#login">
-                Log in
+                    {{ __('Login') }}
                 </a>
+                @if (Route::has('register'))
                 <a class="btn btn-primary text-uppercase font-size-3" href="javacript:" data-toggle="modal" data-target="#signup">
-                Sign up
+                    {{ __('Register') }}
                 </a>
+                @endif
+            @endauth
+            </div>
+            @endif
             </div>
 
             <button class="navbar-toggler btn-close-off-canvas  hamburger-icon border-0" type="button" data-toggle="collapse" data-target="#mobile-menu" aria-controls="mobile-menu" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="hamburger hamburger--squeeze js-hamburger">
             <span class="hamburger-box">
-                <span class="hamburger-inner"></span>
-                </span>
-                </span>
+            <span class="hamburger-inner"></span>
+            </span>
+            </span>
             </button>
 
         </nav>
