36fa1f521baaa54d591d59f83df20c2f868431f1
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-05-23 23:09:39 +0200

    LANG + COOKIE

--- resources/views/scripts/js.blade.php
+++ resources/views/scripts/js.blade.php
@@ -9,6 +9,7 @@
     <script src="{{ asset('assets/plugins/ui-range-slider/jquery-ui.js') }}"></script>
 
     <script src="{{ asset('assets/js/custom.js') }}"></script>
+    {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
 
     <script type="module" src="https://unpkg.com/ionicons@5.4.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule="" src="https://unpkg.com/ionicons@5.4.0/dist/ionicons/ionicons.js"></script>
